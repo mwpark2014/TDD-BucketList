0e2e63c0d7eb6c260846effc233c01cdabfb5f3c
Mason <park.mason.w@gmail.com>
2018-05-17 16:13:20 -0700

    Created create endpoint and a view

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -5,7 +5,8 @@ class Bucketlist(models.Model):
     """This class represents the bucketlist model."""
     name = models.CharField(max_length=255, blank=False, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
-    data_modified = models.DateTimeField(auto_now=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
 
     def __str__(self):
         """Return a human readable representation of the model instance."""
